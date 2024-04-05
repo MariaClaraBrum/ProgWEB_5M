a7735a005f6e15f2bd04dbdfafbd8a683f832e8a
MariaClaraBrum <161535314+MariaClaraBrum@users.noreply.github.com>
2024-04-05 20:31:53 -0300

    Add files via upload

---
+++ LISTA_P1/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu de Atividades JS</title>
+</head>
+<body>
+    <h3>Atividades JavaScript - 5º Módulo</h3>
+    <h5>Maria Clara Assencio Brum</h5>
+    <ul>
+        <li><a href="exercicio1.php">Exercício 1</a></li>
+        <li><a href="exercicio2.php">Exercício 2</a></li>
+        <!--<li><a href="exercicio3.php">Exercício 3</a></li>
+        <li><a href="exercicio4.php">Exercício 4</a></li>
+        <li><a href="exercicio5.php">Exercício 5</a></li> -->
+    </ul>
+</body>
